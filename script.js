1e4c60c972b1a538d7885806c8dc6cdaa1ab6d4d
michefraim <michefraim@gmail.com>
2021-01-24 15:06:29 +0200

    1.0

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const ul = document.querySelector('ul');
+const button = document.querySelector('button');
+const input = document.querySelector('input');
+
+button.onclick = function () {
+    let temp = input.value;
+    input.value = '';
+
+    const li = document.createElement("li");
+    const newButton = document.createElement("button");
+    const span = document.createElement("span");
+
+    li.append(span);
+    li.append(newButton);
+    span.innerText = temp;
+    newButton.innerText = "Delete";
+    ul.append(li);
+
+    newButton.onclick = function () {
+        ul.removeChild(li);
+    };
+    input.focus();
+};
